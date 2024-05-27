ff03dff0eef68a460b157b11e6047f45c067f5c0
dannykhan02 <dannykhan614@gmail.com>
2024-05-27 10:02:13 +0300

    create my file

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,33 @@
-#!/usr/bin/env python3
-
+# shoe.py
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None
+        self.size = size  # Use the setter for validation
+        self.condition = "Used"  # Default condition
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 50:
+            self._brand = value
+        else:
+            raise ValueError("Brand must be a string between 1 and 50 characters.")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
